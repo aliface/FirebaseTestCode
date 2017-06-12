30426c5fbc81c5feec3085510c62ed74c9bbfa27
aliface <balashovaalice@gmail.com>
2017-06-12 09:48:50 -0400

    Create UserInformation.java

---
+++ UserInformation.java
@@ -0,0 +1,18 @@
+package com.example.firebasething;
+
+/**
+ * Created by alice on 6/12/2017.
+ */
+
+public class UserInformation {
+
+    public String name;  //modifiers should always be public!!
+    public String CarType;
+
+    public UserInformation(String name, String carType) {
+        this.name = name;
+        CarType = carType;
+    }
+
+
+}
